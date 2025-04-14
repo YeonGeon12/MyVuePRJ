ccdff4b5db974af9a6c559e7761a5f0eef99bc38
윤건 <yungun200512@gmail.com>
2025-04-14 16:03:40 +0900

    useFetch 컴포저블을 이용한 실습

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
-<!-- src/App.vue -->
 <template>
   <div>
-    <h1>부모</h1>
-    <child-component />
+    <h1>사용자 목록</h1>
+    <button @click="fetchData" :disabled="loading">데이터 가져오기</button>
+    <div v-if="loading">로딩 중...</div>
+    <div v-if="error">에러: {{ error }}</div>
+    <ul v-if="data">
+      <li v-for="user in data" :key="user.id">{{ user.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { provide, ref } from 'vue';
-import ChildComponent from './components/ChildComponent.vue';
+import { useFetch } from './composables/useFetch';
 
 export default {
-  components: { ChildComponent },
   setup() {
-    const theme = ref('light');
-    provide('theme', theme);
+    const { data, error, loading, fetchData } = useFetch(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+
+    return { data, error, loading, fetchData };
   }
 }
 </script>
