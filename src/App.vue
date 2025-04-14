c3b0480b73f5b9cfc18cd576ecb7879623d3778b
윤건 <yungun200512@gmail.com>
2025-04-14 15:59:11 +0900

    Provide/Inject 실습

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <!-- src/App.vue -->
 <template>
   <div>
-    <p>{{ count }}</p>
-    <button @click="increment">증가</button>
+    <h1>부모</h1>
+    <child-component />
   </div>
 </template>
+
 <script>
-import {ref, onBeforeMount, onBeforeUpdate, onBeforeUnmount} from 'vue';
+import { provide, ref } from 'vue';
+import ChildComponent from './components/ChildComponent.vue';
 
 export default {
+  components: { ChildComponent },
   setup() {
-    const count = ref(0);
-    const increment = () => {
-      count.value++;
-    };
-    onBeforeMount(() => {
-      console.log('마운트 전: 초기 설정 준비');
-    });
-    onBeforeUpdate(() => {
-      console.log(`업데이트 전: count는 ${count.value}`);
-    });
-    onBeforeUnmount(() => {
-      console.log('제거 전: 정리 작업');
-    });
-    return {count, increment};
+    const theme = ref('light');
+    provide('theme', theme);
   }
 }
 </script>
+
 <style>
 h1 {
   color: #42b983;
